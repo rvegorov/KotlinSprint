cad61cfdc79ba0ee4c3d9406620f1939852a9d58
REgorov <eogorovromanv@hotmail.com>
2025-01-31 19:36:59 +0300

    KS-1-2 print variables to console

---
+++ src/main/kotlin/lesson_1/lesson_1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main(){
+    val orderCount: Int = 75
+    val thanksText: String = "Благодарим вас за покупку! Чао"
+
+    println(orderCount)
+    println(thanksText)
+
+    var workerCount: Short = 2000
+    //println(workerCount)
+
+    workerCount = 1999
+    println(workerCount)
+}
