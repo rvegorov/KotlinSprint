88bf4f2e1a8f16618192b2c96ae6449e0dbfb8f1
REgorov <eogorovromanv@hotmail.com>
2025-02-01 18:56:51 +0300

    KS-3-2 print citizen data

---
+++ src/main/kotlin/lesson_3/lesson_3_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+    
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val maidenName = "Андреева"
+    val marriageName = "Сидорова"
+    val maidenAge = 20
+    val marriageAge = 22
+
+    println("$maidenName $name $patronymic, $maidenAge")
+    println("$marriageName $name $patronymic, $marriageAge")
+}
