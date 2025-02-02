0ea7f3c42964b4c97bf366159aecfe57dd9f01a3
REgorov <eogorovromanv@hotmail.com>
2025-02-02 11:56:04 +0300

    KS-2-5 fixed percent value

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -4,9 +4,9 @@ import kotlin.math.pow
 
 fun main() {
     val deposit = 70000
-    val yearRate: Double = 0.167
+    val yearRate: Double = 16.7
     val years = 20
 
-    val futureDeposit = deposit * (1 + yearRate).pow(years)
+    val futureDeposit = deposit * (1 + yearRate / 100).pow(years)
     println(String.format("%.3f", futureDeposit))
 }
