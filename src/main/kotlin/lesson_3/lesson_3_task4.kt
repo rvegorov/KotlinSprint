f75a3efac5dea19fbb9cc4dca2b30af5e0d80f03
REgorov <eogorovromanv@hotmail.com>
2025-02-02 12:36:35 +0300

    KS-3-4 moveNumber from 0

--- src/main/kotlin/lesson_3/lesson_3_task4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task4.kt
@@ -3,7 +3,7 @@ package org.example.lesson_3
 fun main() {
     var moveFrom = "E2"
     var moveTo = "E4"
-    var moveNumber = 1
+    var moveNumber = 0
     println("$moveFrom, $moveTo, $moveNumber")
 
     moveFrom = "D2"
