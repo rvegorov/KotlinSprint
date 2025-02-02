a654599f1d66daf0290aa97f70fd6e9f8bd60c22
REgorov <eogorovromanv@hotmail.com>
2025-02-02 11:52:50 +0300

    KS-2-4 fixed percent value

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -3,10 +3,10 @@ package org.example.lesson_2
 fun main() {
     val minedCrystal = 7
     val minedIron = 11
-    val buffBonus = 0.2f
+    val buffBonusPercent = 20
 
-    val bonusCrystal = (minedCrystal * buffBonus).toInt()
-    val bonusIron = (minedIron * buffBonus).toInt()
+    val bonusCrystal = (minedCrystal * buffBonusPercent / 100).toInt()
+    val bonusIron = (minedIron * buffBonusPercent / 100).toInt()
 
     println(bonusCrystal)
     println(bonusIron)
