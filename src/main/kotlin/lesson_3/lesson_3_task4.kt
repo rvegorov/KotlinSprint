ccfda7f383864bb9cd1f09dc88b2052824f68b81
REgorov <eogorovromanv@hotmail.com>
2025-02-02 12:38:54 +0300

    KS-3-4 fixed string format

--- src/main/kotlin/lesson_3/lesson_3_task4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task4.kt
@@ -4,10 +4,10 @@ fun main() {
     var moveFrom = "E2"
     var moveTo = "E4"
     var moveNumber = 0
-    println("$moveFrom, $moveTo, $moveNumber")
+    println("$moveFrom-$moveTo;$moveNumber")
 
     moveFrom = "D2"
     moveTo = "D3"
     moveNumber++
-    println("$moveFrom, $moveTo, $moveNumber")
+    println("$moveFrom-$moveTo;$moveNumber")
 }
