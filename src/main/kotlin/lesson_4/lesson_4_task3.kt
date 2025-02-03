3441232eacea09c54c42fd980cb6aad2bf0a2d1f
REgorov <eogorovromanv@hotmail.com>
2025-02-03 20:04:37 +0300

    KS-5-1 create if/else bot check

--- src/main/kotlin/lesson_4/lesson_4_task3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task3.kt
@@ -2,11 +2,13 @@ package org.example.lesson_4
 
 fun main() {
     val isSunny: Boolean = true
-    val tentIsOpen: Boolean = true
+    val isAwningOpen: Boolean = true
     val humidity: Int = 20
     val season: String = "зима"
+
     val conditionsAreGood: Boolean =
-        (isSunny == GOOD_IS_SUNNY && tentIsOpen == GOOD_TENT_IS_OPEN && humidity == GOOD_HUMIDITY && season != BAD_SEASON)
+        (isSunny == GOOD_IS_SUNNY && isAwningOpen == GOOD_TENT_IS_OPEN && humidity == GOOD_HUMIDITY && season != BAD_SEASON)
+
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsAreGood")
 }
 
