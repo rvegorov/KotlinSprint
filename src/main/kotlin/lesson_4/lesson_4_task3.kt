c9339d389fd9d1a7902875887eda57d0099cff3c
REgorov <eogorovromanv@hotmail.com>
2025-02-03 20:06:15 +0300

    KS-5-1 refactor boolean names

--- src/main/kotlin/lesson_4/lesson_4_task3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task3.kt
@@ -7,12 +7,12 @@ fun main() {
     val season: String = "зима"
 
     val conditionsAreGood: Boolean =
-        (isSunny == GOOD_IS_SUNNY && isAwningOpen == GOOD_TENT_IS_OPEN && humidity == GOOD_HUMIDITY && season != BAD_SEASON)
+        (isSunny == GOOD_IS_SUNNY && isAwningOpen == GOOD_IS_AWNING_OPEN && humidity == GOOD_HUMIDITY && season != BAD_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsAreGood")
 }
 
 const val GOOD_IS_SUNNY = true
-const val GOOD_TENT_IS_OPEN = true
+const val GOOD_IS_AWNING_OPEN = true
 const val GOOD_HUMIDITY = 20
 const val BAD_SEASON = "зима"
