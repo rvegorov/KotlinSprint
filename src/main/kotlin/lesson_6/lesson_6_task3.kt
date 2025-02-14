ad4663174a5309acf8a1e0545c4a1fdd5fa3cf92
REgorov <eogorovromanv@hotmail.com>
2025-02-14 11:04:25 +0300

    KS-6-3 reformat code

--- src/main/kotlin/lesson_6/lesson_6_task3.kt
+++ src/main/kotlin/lesson_6/lesson_6_task3.kt
@@ -4,8 +4,7 @@ fun main() {
     println("Введите количество секунд:")
     var timer = readln().toInt()
 
-    while (timer>0)
-    {
+    while (timer > 0) {
         println("Осталось секунд: ${timer--}")
         Thread.sleep(1000)
     }
