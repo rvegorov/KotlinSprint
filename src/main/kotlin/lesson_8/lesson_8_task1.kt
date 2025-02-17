e6aeee9b3cec1fb1bb88a8abc32200d19f6ae119
REgorov <eogorovromanv@hotmail.com>
2025-02-17 10:54:34 +0300

    KS-8-1 shorten the code

--- src/main/kotlin/lesson_8/lesson_8_task1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task1.kt
@@ -2,10 +2,5 @@ package org.example.lesson_8
 
 fun main() {
     val dailyViews = intArrayOf(85, 100, 98, 110, 92, 66, 60)
-
-    var totalViews = 0
-    for (views in dailyViews) {
-        totalViews += views
-    }
-    println(totalViews)
+    println(dailyViews.sum())
 }
