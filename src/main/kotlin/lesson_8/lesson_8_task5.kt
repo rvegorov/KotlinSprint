6f097e95e88aacb16fbcafc4332d533cb3d46cc1
REgorov <eogorovromanv@hotmail.com>
2025-02-17 11:09:49 +0300

    KS-8-5 shorten the code

--- src/main/kotlin/lesson_8/lesson_8_task5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task5.kt
@@ -3,11 +3,11 @@ package org.example.lesson_8
 fun main() {
     println("Введите количество ингредиентов:")
     val arraySize = readln().toInt()
-
     val recipeArray = arrayOfNulls<String>(arraySize)
+
     println("Введите ингредиенты по одному:")
-    for (i in recipeArray) {
-        recipeArray[recipeArray.indexOf(i)] = readln()
+    for (i in recipeArray.indices) {
+        recipeArray[i] = readln()
     }
 
     println("Ваш рецепт:")
