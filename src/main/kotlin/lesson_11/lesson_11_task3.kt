0387706bbddde61e15818ca71eadf3943086d9b9
REgorov <eogorovromanv@hotmail.com>
2025-02-19 13:02:43 +0300

    KS-11-3 create chat classes

---
+++ src/main/kotlin/lesson_11/lesson_11_task3.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_11
+
+class RoomUser(
+    //val id: Int,
+    val name: String,
+    val avatar: String = "blank.jpg",
+    var status: String = "разговаривет"
+) {
+}
+
+class Room(
+    val roomName: String,
+    val users: MutableList<RoomUser>,
+    val roomCover: String = "empty.jpg",
+) {
+    fun addUser(user: RoomUser) {
+        users.add(user)
+    }
+
+    fun changeUserStatus(userName: String, newUserStatus: String) {
+        for (user in users) {
+            if (user.name == userName) user.status = newUserStatus
+        }
+    }
+}
+
+fun main() {
+    val room = Room(
+        roomName = "Ребята", mutableListOf(RoomUser("Олег"), RoomUser("Маша")),
+        "rebyata.png",
+    )
+}
