e6a1c123d8b9bbfe759558f3347a1375dcd1e860
REgorov <eogorovromanv@hotmail.com>
2025-02-25 16:33:59 +0300

    KS-16-3 make password check for User

---
+++ src/main/kotlin/lesson_16/lesson_16_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_16
+
+class User(
+    val name: String,
+    private val password: String,
+) {
+    fun validatePassword(checkPassword: String): Boolean {
+        return (checkPassword == password)
+    }
+}
+
+fun main() {
+    val user = User("Resu", "1111")
+    println(user.validatePassword("2222"))
+}
