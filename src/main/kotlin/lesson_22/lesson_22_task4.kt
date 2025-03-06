418a4d97391e6b3254479d8a2f0b229a62c8478c
REgorov <eogorovromanv@hotmail.com>
2025-03-06 18:26:31 +0300

    KS-22-4 imitate loading MainScreenViewModel

---
+++ src/main/kotlin/lesson_22/lesson_22_task4.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_22
+
+data class MainScreenState(
+    val data: String?,
+    val isLoading: Boolean = false
+)
+
+class MainScreenViewModel(var mainScreenState: MainScreenState) {
+    fun loadData(){
+        mainScreenState =  mainScreenState.copy(data = "отсутствие данных")
+        println(mainScreenState.data)
+        mainScreenState =  mainScreenState.copy(data = "загрузка данных", isLoading = true)
+        println(mainScreenState.data)
+        mainScreenState = mainScreenState.copy(data = "наличие загруженных данных", isLoading = false)
+        println(mainScreenState.data)
+    }
+}
+
+fun main() {
+    val msvm = MainScreenViewModel(MainScreenState("данные"))
+    msvm.loadData()
+}
