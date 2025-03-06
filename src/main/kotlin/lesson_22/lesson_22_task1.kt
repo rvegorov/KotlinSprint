6cea03318680b5b38739075287267f5618eea839
REgorov <eogorovromanv@hotmail.com>
2025-03-06 17:25:18 +0300

    KS-22-1 data vs regular class equals

---
+++ src/main/kotlin/lesson_22/lesson_22_task1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_22
+
+class RegularBook(val title: String, val author: String)
+
+data class DataBook(val title: String, val author: String)
+
+fun main() {
+    val book1 = RegularBook("Ревизор", "Н. Гоголь")
+    val book2 = RegularBook("Ревизор", "Н. Гоголь")
+    val dataBook1 = DataBook("Туман", "С. Кинг")
+    val dataBook2 = DataBook("Туман", "С. Кинг")
+
+    println(book1 == book2)
+    println(dataBook1 == dataBook2)
+
+    // для обычных классов сравниваются ссылки на объекты, а для data данные всех полей класса
+}
